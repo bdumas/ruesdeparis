0c07586f212ad66d5eb095b5cee41b07ad9cd42d
bdumas <benjamin.dumas@gmail.com>
2014-02-11 19:34:36 -0300

    Test git modif

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         
-        /* test meta: {
+        /* TODO meta: {
             banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
               '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
               '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +

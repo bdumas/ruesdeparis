9a495ca9f6eb6bf81e8d0a4b5a5f0d9ffc2df716
bdumas <benjamin.dumas@gmail.com>
2014-05-31 20:38:11 -0300

    auto install

--- app/js/base.js
+++ app/js/base.js
@@ -2,14 +2,7 @@
 if (navigator.mozApps) {
     var checkIfInstalled = navigator.mozApps.getSelf();
     checkIfInstalled.onsuccess = function () {
-        if (checkIfInstalled.result) {
-            // Already installed
-            var installationInstructions = document.querySelector("#installation-instructions");
-            if (installationInstructions) {
-                installationInstructions.style.display = "none";
-            }
-        }
-        else {
+        if (!checkIfInstalled.result) {
             var install = document.querySelector("#install"),
                 manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
             install.className = "show-install";
@@ -29,16 +22,3 @@ if (navigator.mozApps) {
 else {
     console.log("Open Web Apps not supported");
 }
-
-/*console.log(navigator.onLine);
-if (navigator.onLine) {
-    document.write("<script src='http://maps.google.com/maps/api/js?sensor=false'></script>");
-}*/
-
-// Reload content
-/*var reload = document.querySelector("#reload");
-if (reload) {
-    reload.onclick = function () {
-        location.reload(true);
-    };
-}*/

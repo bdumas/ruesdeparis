ee1cb23839f33f8ab51cb3944248f061384153d5
bdumas <benjamin.dumas@gmail.com>
2014-05-31 21:13:48 -0300

    auto install

--- app/js/base.js
+++ app/js/base.js
@@ -1,24 +1,25 @@
-// Install app
-if (navigator.mozApps) {
-    var checkIfInstalled = navigator.mozApps.getSelf();
-    checkIfInstalled.onsuccess = function () {
-        if (!checkIfInstalled.result) {
-            var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-            install.className = "show-install";
-            install.onclick = function () {
-                var installApp = navigator.mozApps.install(manifestURL);
-                installApp.onsuccess = function(data) {
-                    install.style.display = "none";
+(function () {
+    "use strict";
+    // Install app
+    if (navigator.mozApps) {
+        var checkIfInstalled = navigator.mozApps.getSelf();
+        checkIfInstalled.onsuccess = function () {
+            if (!checkIfInstalled.result) {
+                var install = document.querySelector("#install"),
+                    manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+                install.className = "show-install";
+                install.onclick = function () {
+                    var installApp = navigator.mozApps.install(manifestURL);
+                    installApp.onsuccess = function() {
+                        install.style.display = "none";
+                    };
+                    installApp.onerror = function() {
+                        alert("Install failed\n\n:" + installApp.error.name);
+                    };
                 };
-                installApp.onerror = function() {
-                    alert("Install failed\n\n:" + installApp.error.name);
-                    console.log("Install failed\n\n:" + installApp.error.name);
-                };
-            };
-        }
-    };
-}
-else {
-    console.log("Open Web Apps not supported");
-}
+            }
+        };
+    } else {
+        console.log("Open Web Apps not supported");
+    }
+}());
